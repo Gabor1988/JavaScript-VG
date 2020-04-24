173cba423e6f5a639ae3053aa0b03389f49b7c44
Gabor1988 <varga.gabor1988@gmail.com>
2020-04-24 20:38:29 +0200

    insert first part

---
+++ script.js
@@ -0,0 +1,94 @@
+let name = 'Gabor';
+
+console.log(name + "'s apples");
+
+console.log('Hello World!');
+
+//This a JavaScript comment
+
+/*
+    This is a multiline
+    JavaScript comment.
+
+*/
+
+// This is the first part:
+
+// 12+3
+// 15
+// 12-5
+// 7
+// 12*4
+// 48
+// 12/4
+// 3
+// let age
+// undefined
+// age=33
+// 33
+// age/3
+// 11
+// age=12
+// 12
+// age/3
+// 4
+// let legs = 4
+// undefined
+// legs*2
+// 8
+// find=4
+// 4
+// find/2
+// 2
+// legs=5
+// 5
+// let anotherAge = age
+// undefined
+// anotherAge
+// 12
+// let name = 'Gabor'
+// undefined
+// name
+// "Gabor"
+// name = "Peti"
+// "Peti"
+// name
+// "Peti"
+// "Gabor's apples"
+// "Gabor's apples"
+// true
+// true
+// false
+// false
+// let oldEnoughToDrive = false
+// undefined
+// oldEnoughToDrive
+// false
+// oldEnoughToDrive = true
+// true
+// oldEnoughToDrive
+// true
+// name + 'else'
+// "Petielse"
+// name + 'else' + name + age
+// "PetielsePeti12"
+// name + 'else' + 14+ name + age
+// "Petielse14Peti12"
+// 'Adam' - 'apple'
+// NaN
+// true && true
+// true
+// false && true
+// false
+// false && false
+// false
+// true||false
+// true
+// false || true
+// true
+// false || false
+// false
+// true||true
+// true
+// true&&false
+// false
